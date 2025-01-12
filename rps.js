19945a2e3032da0e3900c0cf3316c66f54c97f78
Justin Mallette <86990712+justinmallette@users.noreply.github.com>
2025-01-11 17:29:50 -0800

    Create pseudocode for computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+/* Make Computer choice
+
+CALCULATE a number between 0 and 1
+IF number < .34
+    THEN set the computer's choice to "Rock"
+IF number >= .34 AND < .67
+    THEN set the computer's choice to "Paper"
+ELSE
+    Set the computer's choice to "Scissors"
+*/
+
+
